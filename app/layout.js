1adbfbf441d204bde24d558391e24a430d88375f
Hamza khan <hamzakhan.std25@gmail.com>
2025-11-17 21:15:08 +0500

    setup mainfist files

--- app/layout.js
+++ app/layout.js
@@ -11,18 +11,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata = {
   title: "Finance-Tracker",
   description: "app to store and retrieve finance record",
+  manifest: "/manifest.json",
+  themeColor: "#0f172a"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+
+        {/* iOS Support */}
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}  
+        {children}
       </body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+import withPWAInit from "next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // SW disabled in dev
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+
+  experimental: {
+    optimizePackageImports: ["firebase"],
+  },
+};
+
+export default withPWA(nextConfig);
